bda0cd26949611f77a5fb4b7383f2bae684622b3
Yomnahanyy <153322904+Yomnahanyy@users.noreply.github.com>
2024-03-19 00:38:59 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+let dataNow= new Date();
+let birthDay= document.getElementById("birthday");
+let counter=document. getElementById("btn");
+counter.onclick=function () {
+    let birthDay=new Date(document.getElementById("birthday").value);
+    let datacount=(dataNow - birthDay)/1000/60/60/24/365;
+    let p=decument.querySelector("p");
+    p.innerHTML= `your age is ${parseInt(datacount)}year.`;
+};
+
